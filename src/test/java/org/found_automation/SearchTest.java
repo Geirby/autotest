856fa0bb8cf077c043d042f1546ae216befac0a0
Geirby <m2.suboch@gmail.com>
2021-11-29 16:51:08 +0300

    Create PageObject

--- src/test/java/org/found_automation/SearchTest.java
+++ src/test/java/org/found_automation/SearchTest.java
@@ -1,6 +1,7 @@
 package org.found_automation;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,13 +14,19 @@ public class SearchTest {
     public static SearchPage searchPage;
     public static WebDriver driver;
     public static SearchSteps searchSteps;
-    private static Integer timeOuts = 1000;
+    private static Integer timeOuts = 10000;
+
 
     @BeforeClass
     public static void setup() {
 
-        System.setProperty("webdriver.gecko.driver", ConfProperties.getProperty("moziladriver"));
-        driver = new FirefoxDriver();
+        if(ConfProperties.getProperty("actualBrowser").equalsIgnoreCase("firefox")){
+            System.setProperty("webdriver.gecko.driver", ConfProperties.getProperty("moziladriver"));
+            driver = new FirefoxDriver();
+        }else System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+
+
         searchPage = new SearchPage(driver);
         driver.get(ConfProperties.getProperty("webpage"));
         driver.manage().timeouts().pageLoadTimeout(timeOuts,
